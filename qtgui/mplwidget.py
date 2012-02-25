7a462a6ffe84b5a013080221719d822bcc78c679
Flavio S. Truzzi <flaviotruzzi@gmail.com>
2012-02-25 00:46:46 -0200

    Added Navigation Toolbar

--- qtgui/mplwidget.py
+++ qtgui/mplwidget.py
@@ -1,23 +1,23 @@
 from PyQt4 import QtGui
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
-
-
-class MplCanvas(FigureCanvas):
-	def __init__(self):
-		self.fig = Figure()
-		self.ax = self.fig.add_subplot(111)
-		FigureCanvas.__init__(self, self.fig)
-		FigureCanvas.setSizePolicy(self,QtGui.QSizePolicy.Expanding,QtGui.QSizePolicy.Expanding)
-		FigureCanvas.updateGeometry(self)
-
+from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt4agg import NavigationToolbar2QTAgg as NavigationToolbar
 
 class MplWidget(QtGui.QWidget):
-	def __init__(self, parent = None):
-		QtGui.QWidget.__init__(self, parent)
 
-		self.canvas = MplCanvas()
-		self.vbl = QtGui.QVBoxLayout()
-		self.vbl.addWidget(self.canvas)
-		self.setLayout(self.vbl)
+  def __init__(self, parent = None):
+    QtGui.QWidget.__init__(self, parent)
 
+
+    self.fig = Figure()
+    self.canvas = FigureCanvas(self.fig)
+    self.canvas.setParent(self)
+    self.axes = self.fig.add_subplot(111)
+
+    self.mpl_toolbar = NavigationToolbar(self.canvas, self)
+
+    vbox = QtGui.QVBoxLayout()
+    vbox.addWidget(self.canvas)
+    vbox.addWidget(self.mpl_toolbar)
+    self.setLayout(vbox)
--- qtgui/sharingan.py
+++ qtgui/sharingan.py
@@ -8,37 +8,35 @@ import scipy.misc
 
 class DesignerMainWindow(QtGui.QMainWindow, Ui_MainWindow):
 
-	def __init__(self, parent = None):
-		super(DesignerMainWindow, self).__init__(parent)
-		self.setupUi(self)
+    def __init__(self, parent = None):
+        super(DesignerMainWindow, self).__init__(parent)
+        self.setupUi(self)
 
-		QtCore.QObject.connect(self.actionLoad_Config, QtCore.SIGNAL("triggered()"), self.parseConfig)
-		QtCore.QObject.connect(self.actionClose_2, QtCore.SIGNAL('triggered()'), QtGui.qApp, QtCore.SLOT("quit()"))
+        QtCore.QObject.connect(self.actionLoad_Config, QtCore.SIGNAL("triggered()"), self.parseConfig)
+        QtCore.QObject.connect(self.actionClose_2, QtCore.SIGNAL('triggered()'), QtGui.qApp, QtCore.SLOT("quit()"))
 
-	def parseConfig(self):
+    def parseConfig(self):
 #		configFile = str(QtGui.QFileDialog.getOpenFileName())
 #		if file:
 #			self.params = simplejson.load(open(configFile))
 #			image = self.params['root_directory'] + '/frames/' + self.params['filename_format']%int(sys.argv[2])
-		self.refreshImages('image')
+        self.refreshImages('image')
 
 
-	def refreshImages(self, image):
-		imL = [self.im1, self.im2, self.im3, self.im4]
-		for im in imL:
-			im.canvas.ax.clear()
-			im.canvas.ax.imshow(scipy.misc.lena(), cmap=pylab.cm.bone, interpolation='nearest')
-			im.canvas.draw()
+    def refreshImages(self, image):
+        imL = [self.im1, self.im2, self.im3, self.im4]
+        for im in imL:
+            im.axes.clear()
+            im.axes.imshow(scipy.misc.lena(), cmap=pylab.cm.bone,
+                interpolation='nearest')
+            im.canvas.draw()
+        self.cid = self.im1.canvas.mpl_connect('button_press_event', self.onclick)
 
-		self.cid = self.im1.canvas.mpl_connect('button_press_event', self.onclick)
-
-	def onclick(self,event):
-		print 'button=%d, x=%d, y=%d, xdata=%f, ydata=%f'%(event.button, event.x, event.y, event.xdata, event.ydata)
+    def onclick(self,event):
+        print 'button=%f, x=%f, y=%f, xdata=%f, ydata=%f'%(event.button, event.x, event.y, event.xdata, event.ydata)
 
 app = QtGui.QApplication(sys.argv)
 
 dmw = DesignerMainWindow()
 dmw.show()
 sys.exit(app.exec_())
-
-
